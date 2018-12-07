2a1e28f6fd80d584a84cd3fc00d2b3d3cfea1b3c
Kenji Abe <kenji.01.star@gmail.com>
2018-12-07 23:06:30 +0900

    Update 1.0.0-alpha08

--- app/src/main/java/com/star_zero/navigation_keep_fragment_sample/navigation/KeepStateNavigator.kt
+++ app/src/main/java/com/star_zero/navigation_keep_fragment_sample/navigation/KeepStateNavigator.kt
@@ -3,13 +3,14 @@ package com.star_zero.navigation_keep_fragment_sample.navigation
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.FragmentManager
+import androidx.navigation.NavDestination
 import androidx.navigation.NavOptions
 import androidx.navigation.Navigator
 import androidx.navigation.fragment.FragmentNavigator
 
 @Navigator.Name("keep_state_fragment") // `keep_state_fragment` is used in navigation xml
 class KeepStateNavigator(
-    context: Context,
+    private val context: Context,
     private val manager: FragmentManager, // Should pass childFragmentManager.
     private val containerId: Int
 ) : FragmentNavigator(context, manager, containerId) {
@@ -19,8 +20,7 @@ class KeepStateNavigator(
         args: Bundle?,
         navOptions: NavOptions?,
         navigatorExtras: Navigator.Extras?
-    ) {
-
+    ): NavDestination? {
         val tag = destination.id.toString()
         val transaction = manager.beginTransaction()
 
@@ -31,7 +31,8 @@ class KeepStateNavigator(
 
         var fragment = manager.findFragmentByTag(tag)
         if (fragment == null) {
-            fragment = destination.createFragment(args)
+            val className = destination.className
+            fragment = instantiateFragment(context, manager, className, args)
             transaction.add(containerId, fragment, tag)
         } else {
             transaction.attach(fragment)
@@ -41,6 +42,6 @@ class KeepStateNavigator(
         transaction.setReorderingAllowed(true)
         transaction.commit()
 
-        dispatchOnNavigatorNavigated(destination.id, BACK_STACK_DESTINATION_ADDED)
+        return null
     }
 }
