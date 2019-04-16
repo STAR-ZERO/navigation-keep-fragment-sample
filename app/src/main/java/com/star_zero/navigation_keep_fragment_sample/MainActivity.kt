a2ddf83d5d55d4bf676dd0d95c326b1f1a234e7d
Decky Fx <decky@itprovent.com>
2019-04-16 11:39:14 +0700

    Using One navgraph
    - Attempt to use one navgraph for whole navigations
    - Solve crash issue when navigate after popBackStack
    - Keep state of navigations even after switch tab

--- app/src/main/java/com/star_zero/navigation_keep_fragment_sample/MainActivity.kt
+++ app/src/main/java/com/star_zero/navigation_keep_fragment_sample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.star_zero.navigation_keep_fragment_sample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.plusAssign
 import androidx.navigation.ui.setupWithNavController
@@ -15,28 +16,34 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        this.navController = findNavController(R.id.nav_host_fragment)
 
         // get fragment
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)!!
 
         // setup custom navigator
-        val navigator = KeepStateNavigator(this, navHostFragment.childFragmentManager, R.id.nav_host_fragment)
-        navController.navigatorProvider += navigator
-
+        //val navigator = KeepStateNavigator(this, navHostFragment.childFragmentManager, R.id.nav_host_fragment)
+        //navController.navigatorProvider += navigator
         // set navigation graph
-        navController.setGraph(R.navigation.nav_graph)
-
-        binding.bottomNav.setupWithNavController(navController)
+        //navController.setGraph(R.navigation.nav_graph)
+        // binding.bottomNav.setupWithNavController(navController)
 
         // With BackStack
-//        val navigator = KeepStateBackStackNavigator(this, navHostFragment.childFragmentManager, R.id.nav_host_fragment)
-//        navController.navigatorProvider += navigator
-//        navController.setGraph(R.navigation.nav_graph_back_stack)
-//        binding.bottomNav.setupWithNavController(navController, navigator)
+        val navigator = KeepStateBackStackNavigator(this, navHostFragment.childFragmentManager, R.id.nav_host_fragment)
+        this.navController.navigatorProvider += navigator
+        // set navigation graph
+        this.navController.setGraph(R.navigation.nav_graph_back_stack)
+        binding.bottomNav.setupWithNavController(this.navController, navigator)
+    }
+
+    override fun onBackPressed() {
+        this.navController.popBackStack(this.navController.currentDestination!!.id, true)
+        super.onBackPressed()
     }
 }
--- app/src/main/java/com/star_zero/navigation_keep_fragment_sample/navigation/KeepStateBackStackNavigator.kt
+++ app/src/main/java/com/star_zero/navigation_keep_fragment_sample/navigation/KeepStateBackStackNavigator.kt
@@ -1,9 +1,9 @@
 package com.star_zero.navigation_keep_fragment_sample.navigation
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import android.util.Log.i
+import androidx.core.view.forEach
 import androidx.fragment.app.FragmentManager
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
@@ -11,8 +11,14 @@ import androidx.navigation.NavOptions
 import androidx.navigation.Navigator
 import androidx.navigation.fragment.FragmentNavigator
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import java.lang.IllegalArgumentException
 import java.util.*
+import kotlin.collections.MutableList
+import kotlin.collections.count
+import kotlin.collections.forEach
+import kotlin.collections.last
+import kotlin.collections.mutableListOf
+import kotlin.collections.set
+import kotlin.collections.toIntArray
 
 // With BackStack
 // TODO: This is just sample. I don't recommend using this in production.
@@ -25,24 +31,25 @@ class KeepStateBackStackNavigator(
 
     private var popBackStackListener: ((Int) -> Unit)? = null
 
+    // this variable will be used to store backstacks list instead of super class mBackStack
     private val backStack = Stack<Int>()
 
     private val onBackStackChangedListener = FragmentManager.OnBackStackChangedListener {
-
-        if (backStack.count() > manager.backStackEntryCount + 1) {
-            backStack.pop()
-            popBackStackListener?.invoke(backStack.peek())
+        if (this.backStack.count() > this.manager.backStackEntryCount + 1) {
+            this.backStack.pop()
+            this.popBackStackListener?.invoke(backStack.peek())
+        } else {
         }
     }
 
     override fun onBackPressAdded() {
         super.onBackPressAdded()
-        manager.addOnBackStackChangedListener(onBackStackChangedListener)
+        this.manager.addOnBackStackChangedListener(onBackStackChangedListener)
     }
 
     override fun onBackPressRemoved() {
         super.onBackPressRemoved()
-        manager.removeOnBackStackChangedListener(onBackStackChangedListener)
+        this.manager.removeOnBackStackChangedListener(onBackStackChangedListener)
     }
 
     override fun navigate(
@@ -52,62 +59,93 @@ class KeepStateBackStackNavigator(
         navigatorExtras: Navigator.Extras?
     ): NavDestination? {
         val tag = destination.id.toString()
-        val transaction = manager.beginTransaction()
+        val transaction = this.manager.beginTransaction()
 
-        val currentFragment = manager.primaryNavigationFragment
+        val currentFragment = this.manager.primaryNavigationFragment
         if (currentFragment != null) {
             transaction.detach(currentFragment)
         }
 
-        var fragment = manager.findFragmentByTag(tag)
+        var fragment = this.manager.findFragmentByTag(tag)
         if (fragment == null) {
             val className = destination.className
-            fragment = instantiateFragment(context, manager, className, args)
-            transaction.add(containerId, fragment, tag)
+            fragment = this.instantiateFragment(context, manager, className, args)
+            transaction.add(this.containerId, fragment, tag)
         } else {
             transaction.attach(fragment)
         }
 
-        if (!backStack.isEmpty()) {
+        //
+        if (!this.backStack.isEmpty()) {
             transaction.addToBackStack(tag)
         }
 
-        backStack.push(destination.id)
+        this.backStack.push(destination.id)
 
         transaction.setPrimaryNavigationFragment(fragment)
         transaction.setReorderingAllowed(true)
         transaction.commit()
-
         return destination
     }
 
+    override fun popBackStack(): Boolean {
+        if (this.backStack.isEmpty()) {
+            return false
+        }
+        if (this.manager.isStateSaved) {
+            return false
+        }
+        if (this.manager.backStackEntryCount > 0) {
+
+        } // else, we're on the first Fragment, so there's nothing to pop from FragmentManager
+        this.backStack.pop()
+        return true
+    }
+
     fun setPopBackStackListener(listener: (Int) -> Unit) {
-        popBackStackListener = listener
+        this.popBackStackListener = listener
     }
 
     // For rotation
     override fun onSaveState(): Bundle? {
         val bundle = super.onSaveState() ?: Bundle()
-        bundle.putIntArray(KEY_BACK_STACK_IDS, backStack.toIntArray())
+        bundle.putIntArray(KEY_BACK_STACK_IDS, this.backStack.toIntArray())
         return bundle
     }
 
     override fun onRestoreState(savedState: Bundle?) {
         super.onRestoreState(savedState)
         savedState?.getIntArray(KEY_BACK_STACK_IDS)?.forEach {
-            backStack.push(it)
+            this.backStack.push(it)
         }
     }
 
     companion object {
         private const val KEY_BACK_STACK_IDS = "back_stack_ids"
     }
+
+    val lastVisitedDestinationId: Int
+        get() = this.backStack.peek()
 }
 
+@SuppressLint("UseSparseArrays")
 fun BottomNavigationView.setupWithNavController(navController: NavController, navigator: KeepStateBackStackNavigator) {
+    val pagerBackStack = HashMap<Int, MutableList<Int>>()
+
+    // initialize pager back stack
+    this.menu.forEach {
+        pagerBackStack[it.itemId] = mutableListOf(it.itemId)
+    }
+
     setOnNavigationItemSelectedListener {
+        // we must keep track fragment movement
+        // but don't add destination id, if the last in list are same
+        if ( pagerBackStack[this.selectedItemId]!!.last() != navigator.lastVisitedDestinationId) {
+            pagerBackStack[this.selectedItemId]!!.add(navigator.lastVisitedDestinationId)
+        }
         try {
-            navController.navigate(it.itemId)
+            // navigate to stored hashmap backstack
+            navController.navigate(pagerBackStack[it.itemId]!!.last())
             true
         } catch (e: IllegalArgumentException) {
             false
@@ -116,9 +154,19 @@ fun BottomNavigationView.setupWithNavController(navController: NavController, na
 
     navController.addOnDestinationChangedListener { _, destination, _ ->
         // Change tab
+        // find entry from stack pager lists
+        var tabId : Int = 0
+        pagerBackStack.forEach {
+            if ( it.value.contains(destination.id) ) {
+                tabId = it.key
+            }
+        }
+        if (tabId == 0) {
+            return@addOnDestinationChangedListener
+        }
         (0 until menu.size()).forEach {
             val item = menu.getItem(it)
-            if (item.itemId == destination.id) {
+            if (item.itemId == tabId) {
                 item.isChecked = true
             }
         }
@@ -126,9 +174,22 @@ fun BottomNavigationView.setupWithNavController(navController: NavController, na
 
     navigator.setPopBackStackListener { id ->
         // Press back key
+        // find entry from stack pager lists
+        var tabId : Int = 0
+        pagerBackStack.forEach {
+            if ( it.value.contains(id) ) {
+                tabId = it.key
+            }
+        }
+        if (tabId == 0) {
+            return@setPopBackStackListener
+        }
+        if (pagerBackStack[tabId]!!.size > 1) {
+            pagerBackStack[tabId]!!.remove(pagerBackStack[tabId]!!.size - 1)
+        }
         (0 until menu.size()).forEach {
             val item = menu.getItem(it)
-            if (item.itemId == id) {
+            if (item.itemId == tabId) {
                 item.isChecked = true
             }
         }
